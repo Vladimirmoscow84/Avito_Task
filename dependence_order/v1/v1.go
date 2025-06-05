16b47740032ec44153c6ac6df35aed0d0eaaca20
Vladimirmoscow84 <ccr1@yandex.ru>
2025-06-05 17:09:38 +0300

    dependence_order

---
+++ dependence_order/v1/v1.go
@@ -0,0 +1,61 @@
+package main
+
+import "github.com/goforj/godump"
+
+type stack []string
+
+func (s *stack) isEmpty() bool {
+	return len(*s) == 0
+}
+
+func (s *stack) push(value string) {
+	(*s) = append((*s), value)
+}
+
+func (s *stack) pop() (string, bool) {
+	if s.isEmpty() {
+		return "", false
+	}
+	value := (*s)[len(*s)-1]
+	*s = (*s)[:len(*s)-1]
+	return value, true
+}
+
+func main() {
+	deps := map[string][]string{
+		"tensorflow": {"nvcc", "gpu", "linux"},
+		"nvcc":       {"linux"},
+		"linux":      {"core"},
+		"mylib":      {"tensorflow"},
+		"mylib2":     {"requests"},
+	}
+	stack := stack{}
+	color := make(map[string]string)
+	res := make([]string, 0)
+	for key := range deps {
+		stack.push(key)
+		color[key] = "white"
+		for _, v := range deps[key] {
+			color[v] = "white"
+		}
+	}
+	for !stack.isEmpty() {
+		key, _ := stack.pop()
+		if color[key] == "white" {
+			stack.push(key)
+			color[key] = "gray"
+			data, ok := deps[key]
+			if ok && len(data) > 0 {
+				for _, v := range deps[key] {
+					if color[v] == "white" {
+						stack.push(v)
+					}
+				}
+			}
+		} else if color[key] == "gray" {
+			res = append(res, key)
+			color[key] = "black"
+		}
+	}
+	godump.Dump(res)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module Avito_Task
 
-go 1.22.5
+go 1.23.1
+
+toolchain go1.24.0
+
+require github.com/goforj/godump v1.1.0
