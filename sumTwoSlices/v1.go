35375c94fd2a0477d803f79b8695372791ed0c82
Vladimirmoscow84 <ccr1@yandex.ru>
2025-06-17 02:43:12 +0300

    sumTwoSlices v1 done

--- sumTwoSlices/v1.go
+++ sumTwoSlices/v1.go
@@ -3,15 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var arr1 []int = []int{6, 0, 0}
+	var arr1 []int = []int{5}
 	var arr2 []int = []int{4, 5, 6}
-	if len(arr1) == 0 {
-		fmt.Println(arr2)
-		return
-	} else if len(arr2) == 0 {
-		fmt.Println(arr1)
-		return
-	}
 
 	answer := getSumTwoSlices(arr1, arr2)
 	fmt.Println(answer)
@@ -31,22 +24,18 @@ func getSumTwoSlices(arr1, arr2 []int) []int {
 	//создаем два слайса на основе заданных, но с длиной, равной результирующему, а значения по лишним индексам принимаем 0
 	slice1 := make([]int, length-len(arr1))
 	slice2 := make([]int, length-len(arr2))
-	fmt.Println(slice1)
-	fmt.Println(slice2)
 
 	//заполняем slice1
 	for i := 0; i < len(slice1); i++ {
 		slice1[i] = 0
 	}
 	slice1 = append(slice1, arr1...)
-	fmt.Println(slice1)
 
 	//заполняем slice2
 	for i := 0; i < len(slice2); i++ {
 		slice2[i] = 0
 	}
 	slice2 = append(slice2, arr2...)
-	fmt.Println(slice2)
 
 	//заполняем результирующий слайс
 	n := 0
